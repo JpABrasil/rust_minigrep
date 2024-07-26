b4b875d5fab1d8c07507f3fbdc9943dd934542b3
JpABrasil <joaopedroalexandrino6@hotmail.com>
2024-07-26 16:31:02 -0300

    1.1

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 use std::fs;//Para Ler Arquivos
 use std::error::Error; //Melhorar o feedback de erros
-
+use std::vec;
 pub struct Config{
     pub query: String,
     pub file_path: String,
@@ -21,9 +21,21 @@ impl Config{
 pub fn run(config: Config) -> Result<(),Box<dyn Error>>{ //Função Retorna Result Unit ou Error; Box<dyn Error> é um trait dyn, mais detalhes no capítulo 17
     let contents = fs::read_to_string(config.file_path)?; //? faz a mesma função de panic! e retorna
     //println!("Texto: \n{contents}");
+    for line in search(&config.query,&contents){
+        println!("{line}")
+    }
     Ok(())
 }
 
+pub fn search<'a>(query:&str,contents:&'a str) ->Vec<&'a str>{ //Aqui definimos que os dados retornardos por seach teram o mesmo lifetime de contents
+    let mut results = vec::Vec::new();
+    for line in contents.lines(){//Metodo lines retorna um iterator
+        if line.contains(query){
+            results.push(line)
+        }
+    }
+    results
+}
 #[cfg(test)]
 mod tests{
     use super::*;
@@ -32,18 +44,9 @@ mod tests{
     fn one_result(){
         let query = "duct";
         let contents = "\
-        safe,fast,productive.
-        Pick three.";
+Rust
+safe,fast,productive.
+Pick three.";
         assert_eq!(vec!["safe,fast,productive."],search(query,contents));
-    }
-
-    pub fn search<'a>(query:&str,contents:&'a str) ->Vec<&'a str>{ //Aqui definimos que os dados retornardos por seach teram o mesmo lifetime de contents
-        let mut results = Vec::new();
-        for line in contents.lines(){//Metodo lines retorna um iterator
-            if line.contains(query){
-                results.push(line)
-            }
-        }
-        results
     }
 }
